482cd02b76fdf3cba907074be2b9d342d86b9402
Keith Smith <ksmith.ut@gmail.com>
2014-03-17 21:15:43 -0600

    started work on uglifying

--- test/metalsmith-uglify-test.js
+++ test/metalsmith-uglify-test.js
@@ -1,18 +1,46 @@
 'use strict';
 
-var should = require('should')
+var should     = require('should')
+  , Metalsmith = require('metalsmith')
+  , uglify     = require('../')
+  , uglifyAll
+  , uglifyNone
   ;
+
+// uglifyAll
+// ---------
+uglifyAll = function (done) {
+  Metalsmith('test/uglify-all')
+    .use(uglify())
+    .build(function (err) {
+      should.not.exist(err);
+      // TODO check to see that it built correctly
+      done();
+    });
+};
+
+// uglifyNone
+// ----------
+uglifyNone = function (done) {
+  Metalsmith('test/uglify-none')
+    .use(uglify())
+    .build(function (err) {
+      should.not.exists(err);
+      // TODO check to see that it built correctly
+      done();
+    });
+};
 
 // Tests
 // -----
 describe('metalsmith-uglify tests', function () {
 
   describe('Level 1', function () {
-
+    it('should uglify all .js files', uglifyAll);
   });
 
   describe('Level 2', function () {
-
+    it('should work without any .js files', uglifyNone);
   });
 
 });
