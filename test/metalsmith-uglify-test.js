20f067f661b5bd280d0ffdbfb7a7d97598b210f0
Keith Smith <ksmith.ut@gmail.com>
2014-03-17 20:16:06 -0600

    initial commit

---
+++ test/metalsmith-uglify-test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var should = require('should')
+  ;
+
+// Tests
+// -----
+describe('metalsmith-uglify tests', function () {
+
+  describe('Level 1', function () {
+
+  });
+
+  describe('Level 2', function () {
+
+  });
+
+});
