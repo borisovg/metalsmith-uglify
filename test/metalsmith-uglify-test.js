d73165ca8a37856f423896e139562a8f640bf432
Keith Smith <ksmith.ut@gmail.com>
2014-03-18 10:37:07 -0600

    added initial tests

--- test/metalsmith-uglify-test.js
+++ test/metalsmith-uglify-test.js
@@ -1,11 +1,23 @@
 'use strict';
 
 var should     = require('should')
+  , assertDir  = require('assert-dir-equal')
   , Metalsmith = require('metalsmith')
+  , fs         = require('fs')
   , uglify     = require('../')
+  , isDir
   , uglifyAll
+  , uglifyMap
+  , preserveComments
   , uglifyNone
+  , noData
+  , uglifyError
   ;
+
+isDir = function (dir) {
+  try { fs.statSync(dir).isDirectory(); }
+  catch (exception) { return false; }
+}
 
 // uglifyAll
 // ---------
@@ -14,7 +26,39 @@ uglifyAll = function (done) {
     .use(uglify())
     .build(function (err) {
       should.not.exist(err);
-      // TODO check to see that it built correctly
+      assertDir('test/uglify-all/expected', 'test/uglify-all/build');
+      done();
+    });
+};
+
+// uglifyMap
+// ---------
+uglifyMap = function (done) {
+  var options = {
+    outSourceMap: true
+  };
+
+  Metalsmith('test/uglify-map')
+    .use(uglify(options))
+    .build(function (err) {
+      should.not.exist(err);
+      assertDir('test/uglify-map/expected', 'test/uglify-map/build');
+      done();
+    });
+}
+
+// preserveComments
+// ----------------
+preserveComments = function (done) {
+  var options = {
+    preserveComments: 'all'
+  };
+
+  Metalsmith('test/preserve-comments')
+    .use(uglify(options))
+    .build(function (err) {
+      should.not.exist(err);
+      assertDir('test/preserve-comments/expected', 'test/preserve-comments/build');
       done();
     });
 };
@@ -26,7 +70,41 @@ uglifyNone = function (done) {
     .use(uglify())
     .build(function (err) {
       should.not.exists(err);
-      // TODO check to see that it built correctly
+      assertDir('test/uglify-none/expected', 'test/uglify-none/build');
+      done();
+    });
+};
+
+// noData
+// ------
+noData = function (done) {
+  Metalsmith('test/no-data')
+    .use(function (files, metalsmith, done) {
+      delete files['scripts/main.js'].contents;
+      done();
+    })
+    .use(uglify())
+    .build(function (err) {
+      should(err).exist;
+      should(err.toString()).be.equal(
+        'Error: data for uglify file does not exist: scripts/main.js'
+      );
+      should(isDir(__dirname + '/no-data/build')).be.equal(false);
+      done();
+    });
+};
+
+// uglifyError
+// -----
+uglifyError = function (done) {
+  Metalsmith('test/uglify-error')
+    .use(uglify())
+    .build(function (err) {
+      should(err).exist;
+      should(err.toString()).be.equal(
+        'Error: Uglify: Unexpected token: punc (() in scripts/main.js'
+      );
+      should(isDir(__dirname + '/uglify-error/build')).be.equal(false);
       done();
     });
 };
@@ -37,10 +115,14 @@ describe('metalsmith-uglify tests', function () {
 
   describe('Level 1', function () {
     it('should uglify all .js files', uglifyAll);
+    it('should use a source map', uglifyMap);
+    it('should preserve all comments', preserveComments);
   });
 
   describe('Level 2', function () {
     it('should work without any .js files', uglifyNone);
+    it('should handle data not being passed', noData);
+    it('should handle an uglify error', uglifyError);
   });
 
 });
--- test/uglify-map/src/scripts/main.js
+++ test/uglify-map/src/scripts/main.js
@@ -1,7 +1,7 @@
 (function () {
 
   function testFunction(param1, param2, param3) {
-    return param1 + param3;
+    return param1 + param2;
   };
 
   console.log(testFunction('used', 'not used', 'used'));
