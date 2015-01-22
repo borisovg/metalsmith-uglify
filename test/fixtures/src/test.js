fac718b2619dc6ef235c908297fa834f4f8cb360
Keith Smith <ksmith.ut@gmail.com>
2015-01-22 13:28:55 -0700

    added additional tests

--- test/fixtures/src/test.js
+++ test/fixtures/src/test.js
@@ -1,11 +1,15 @@
 (function ($) {
-
+  // %'all'%
   var plugin = {
     init: function (selector) {
       $(selector).doSomething();
     }
   };
-
+  //! %'some'% %'all'%
+  // @preserve %'some'% %'all'%
+  // @license %'some'% %'all'%
+  // @cc_on %'some'% %'all'%
+  // %'custom'%
   $(function () {
     plugin.init();
   });
--- test/metalsmith-uglify-test.js
+++ test/metalsmith-uglify-test.js
@@ -37,4 +37,65 @@ describe('metalsmith-uglify', function () {
       });
   });
 
+  it('should preserve all comments', function (done) {
+    var build = new Metalsmith(FIXTURES)
+      .use(uglify({
+        preserveComments: 'all'
+      }))
+      .build(function (err, files) {
+        if (err) { return done(err); }
+        var ALL_REGEX = /(%'all'%)/g;
+        var origContents = String(files['test.js'].contents);
+        var minContents  = String(files['test.min.js'].contents);
+        expect(origContents.match(ALL_REGEX))
+          .to.be.eql(minContents.match(ALL_REGEX));
+        done();
+      });
+  });
+
+  it('should preserve some comments', function (done) {
+    var build = new Metalsmith(FIXTURES)
+      .use(uglify({
+        preserveComments: 'some'
+      }))
+      .build(function (err, files) {
+        if (err) { return done(err); }
+        var SOME_REGEX = /(%'some'%)/g;
+        var origContents = String(files['test.js'].contents);
+        var minContents  = String(files['test.min.js'].contents);
+        expect(origContents.match(SOME_REGEX))
+          .to.be.eql(minContents.match(SOME_REGEX));
+        done();
+      });
+  });
+
+  it('should preserve contents by function', function (done) {
+    var build = new Metalsmith(FIXTURES)
+      .use(uglify({
+        preserveComments: function (node, comment) {
+          return comment.value.indexOf('custom') !== -1;
+        }
+      }))
+      .build(function (err, files) {
+        if (err) { return done(err); }
+        var CUSTOM_REGEX = /(%'custom'%)/g;
+        var origContents = String(files['test.js'].contents);
+        var minContents  = String(files['test.min.js'].contents);
+        expect(origContents.match(CUSTOM_REGEX))
+          .to.be.eql(minContents.match(CUSTOM_REGEX));
+        done();
+      });
+  });
+
+  it('should catch the error gracefully', function (done) {
+    var build = new Metalsmith(FIXTURES)
+      .use(uglify({
+        filter: '**/*.jsx'
+      }))
+      .build(function (err, files) {
+        expect(err).to.be.an(Error);
+        done();
+      });
+  });
+
 });
